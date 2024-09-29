404497e1b6b2147714f67fd7b80dc70a56d2078f
SumayahAbdullahQ <sumayahabdullah27@gmail.com>
2024-09-29 16:33:41 +0300

    Initial Commit

---
+++ SignCards/SignCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignCardsApp.swift
+//  SignCards
+//
+//  Created by Sumayah Alqahtani on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct SignCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
