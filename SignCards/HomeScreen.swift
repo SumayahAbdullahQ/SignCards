3331f9c91f7c097b1cca18ceabb1284972407680
SumayahAbdullahQ <sumayahabdullah27@gmail.com>
2024-09-29 16:39:02 +0300

    Screen(home-card-folder)

---
+++ SignCards/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  SignCards
+//
+//  Created by Sumayah Alqahtani on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
---
+++ SignCards/NewFolderScreen.swift
@@ -0,0 +1,18 @@
+//
+//  NewFolderScreen.swift
+//  SignCards
+//
+//  Created by Sumayah Alqahtani on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct NewFolderScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewFolderScreen()
+}
