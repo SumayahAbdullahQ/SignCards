e1dbd44ce3e28c2a2954e66141859f59c60574c9
SumayahAbdullahQ <sumayahabdullah27@gmail.com>
2024-09-30 15:18:19 +0300

    new card screen

--- SignCards/AddCardScreen.swift
+++ SignCards/AddCardScreen.swift
@@ -9,7 +9,74 @@ import SwiftUI
 
 struct AddCardScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Button{ }
+            label:{
+                Text("Cansel")
+                    .padding(.horizontal)
+            }
+                
+                Spacer()
+                
+                Button{ }
+            label:{
+                Text("Done")
+                    .padding(.horizontal)
+            }
+            }
+            HStack{
+                
+                Text("New Card")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.vertical, 1)
+                    .padding(.horizontal)
+                Spacer()
+            }
+            Rectangle()
+                .fill(.white)
+                .frame(width: 300, height: 200)
+                .border(.gray, width: 1.5)
+                .padding()
+            HStack(spacing: 10)
+            {
+                Circle()
+                    .fill(.red)
+                    .frame(width: 60, height: 60)
+                Circle()
+                    .fill(.orange)
+                    .frame(width: 60, height: 60)
+                Circle()
+                    .fill(.yellow)
+                    .frame(width: 60, height: 60)
+                Circle()
+                    .fill(.brown)
+                    .frame(width: 60, height: 60)
+                Circle()
+                    .fill(.gray)
+                    .frame(width: 60, height: 60)
+            }
+            HStack(spacing: 10)
+            {
+                Circle()
+                    .fill(.green)
+                    .frame(width: 60, height: 60)
+                Circle()
+                    .fill(.mint)
+                    .frame(width: 60, height: 60)
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 60, height: 60)
+                Circle()
+                    .fill(.indigo)
+                    .frame(width: 60, height: 60)
+                Circle()
+                    .fill(.purple)
+                    .frame(width: 60, height: 60)
+            }
+            Spacer()
+        }
     }
 }
 
