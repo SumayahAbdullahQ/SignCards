6090d3d973ce207563a05aa13cfdeef251262d3d
Walaa <Walaa.matar20@gmail.com>
2024-09-30 16:39:37 +0300

    update

---
+++ SignCards/HomeScreen2.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen2.swift
+//  SignCards
+//
+//  Created by Walaa on 30/09/2024.
+//
+
+import SwiftUI
+
+struct HomeScreen2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen2()
+}
